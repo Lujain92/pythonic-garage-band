99e11e8f9a875b776661b4f4d6f3003edd69c733
Lujain92 <lujainaljarrah92@gmail.com>
2022-10-12 11:52:02 -0700

    done

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,10 +1,52 @@
 class Band:
-    pass
+    instances=[]
+    def __init__(self,name,members=[]):
+        self.name=name
+        self.members=members
+        Band.instances.append(self)
+    def __len__(self):
+        return    
+    def play_solos(self):
+        return(["face melting guitar solo","bom bom buh bom","rattle boom crash"])
+    @staticmethod    
+    def to_list():
+        return Band.instances
 class Musician:
-    pass
-class Guitarist:
-    pass
-class Bassist:
-    pass
-class Drummer:
-    pass
+    def __init__(self,name):
+        self.name=name
+    
+class Guitarist(Musician,Band):
+    def __init__(self,name):
+        super().__init__(name)
+    def __str__(self):
+        return(f"My name is {self.name} and I play guitar")
+    def __repr__(self):
+        return(f"Guitarist instance. Name = {self.name}")
+    def get_instrument(self):
+        return("guitar")
+    def play_solo(self):
+        return(self.play_solos()[0])
+
+
+class Bassist(Musician,Band):
+    def __init__(self,name):
+        super().__init__(name)
+    def __str__(self):
+        return(f"My name is {self.name} and I play bass")
+    def __repr__(self):
+        return(f"Bassist instance. Name = {self.name}")
+    def get_instrument(self):
+        return("bass")
+    def play_solo(self):
+        return(self.play_solos()[1])
+class Drummer(Musician,Band):
+    def __init__(self,name):
+        super().__init__(name)
+    def __str__(self):
+        return(f"My name is {self.name} and I play drums")
+    def __repr__(self):
+        return(f"Drummer instance. Name = {self.name}")
+    def get_instrument(self):
+        return("drums")
+    def play_solo(self):
+        return(self.play_solos()[2])   
